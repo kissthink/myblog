91aa189c6b4ae37b22cf00eba70331e09b68902d
zhaojunmeng <zhaojun.meng@gmail.com>
2013-02-02 20:18:11 +0800

    Add Google Analytics.

--- conf.py
+++ conf.py
@@ -277,6 +277,18 @@ SEARCH_FORM = ""
 # Google analytics or whatever else you use. Added to the bottom of <body>
 # in the default template (base.tmpl).
 ANALYTICS = """
+<!-- Start of Google Analytics -->
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-38185127-1']);
+  _gaq.push(['_trackPageview']);
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
+<!-- End of Google Analytics -->
     """
 
 # The possibility to extract metadata from the filename by using a
